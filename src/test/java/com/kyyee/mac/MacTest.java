9a80e09d2cf16a725b9b47fd41c18739ef9b8d42
kyyee <kyyeeyoung@qq.com>
2017-06-21 20:03:09 +0800

    new test
    Signed-off-by: kyyee <kyyeeyoung@qq.com>

--- src/test/java/com/kyyee/mac/MacTest.java
+++ src/test/java/com/kyyee/mac/MacTest.java
@@ -9,10 +9,21 @@ public class MacTest {
     @Test
     public void randomMacTest() {
         Mac mac = new Mac();
-        System.out.println("randomMac:       " + mac.randomMac());
-        Integer[] oui = {0x32, 0x21, 0xa2};
-        System.out.println("randomMacWithOui:" + mac.randomMacWithOui(oui));
-        System.out.println("randomMac4Xen:   " + mac.randomMac4Xen());
-        System.out.println("randomMac4Qemu:  " + mac.randomMac4Qemu());
+        for (int i = 0; i < 5; i++) {
+            System.out.println("randomMac:       " + mac.randomMac());
+        }
+        System.out.println("------------------->");
+        for (int i = 0; i < 5; i++) {
+            Integer[] oui = {0x32, 0x21, 0xa2};
+            System.out.println("randomMacWithOui:" + mac.randomMacWithOui(oui));
+        }
+        System.out.println("------------------->");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("randomMac4Xen:   " + mac.randomMac4Xen());
+        }
+        System.out.println("------------------->");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("randomMac4Qemu:  " + mac.randomMac4Qemu());
+        }
     }
 }
